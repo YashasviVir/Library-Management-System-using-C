ec774553438f12740e40a02e37e900730ec9bbff
Yashasvi Virani <yrvirani@gmail.com>
2022-12-17 17:31:31 +0530

    Add files via upload

---
+++ LIST.c
@@ -0,0 +1,33 @@
+LIST OF BOOKS
+
+1. NAME - Sherlock Holmes
+AUTHOR - Sir Arthur Conan Doyle
+2. NAME - To Kill A Mockingbird
+AUTHOR - Harper Lee
+3. NAME - Jane Eyre
+AUTHOR - Charlotte Bronte
+4. NAME - People We Meet On Vacation
+AUTHOR - Sally Rooney
+5. NAME - The Perfect Murder
+AUTHOR - Ruskin Bond
+6. NAME - Metamorphosis
+AUTHOR - Frank Kafka
+7. NAME - Norweigian Wood
+AUTHOR - Haruki Murakami
+8. NAME - Hamlet
+AUTHOR - Shakespeare
+9. NAME - Of Mice And Men
+AUTHOR - Shakespeare
+10. NAME - Wuthering Heights
+AUTHOR - Emily Bronte
+11. NAME - Pride and Prejudice
+AUTHOR - Jane Austen
+12. NAME - The Great Gatsby
+AUTHOR - F. Scott Fitzgerald
+13. NAME - The Book Thief
+AUTHOR - Markus Zusak
+14. NAME - 1984
+AUTHOR - George Orwell
+15. NAME - Hamnet
+AUTHOR - Maggie O Ferrel
+
